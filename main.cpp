d6596c45aa13a32fb6ae87de92802c053e10b255
Matt Kirwan <matthewkirwan@gmail.com>
2013-07-13 14:54:06 +0100

    Removed break loop now that we have a little more control  with the game ties

--- main.cpp
+++ main.cpp
@@ -6,25 +6,13 @@ int main()
 	TicTacToe Game;
 	
 	Game.welcome();
-
 	Game.drawBoard();
-
-	int i = 0;
 	
 	do
 	{
-		if(i > 10)
-		{
-			break;
-		}
-
 		Game.makeMove();
-
 		Game.drawBoard();
-
 		Game.nextTurn();
-
-		i++;
 	}
 	while( !Game.isTie() );
 
