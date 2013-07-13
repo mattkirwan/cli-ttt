b309c5263178b59e65910ce25f3d25d63e4ec9a3
Matt Kirwan <matthewkirwan@gmail.com>
2013-07-13 11:35:58 +0100

    Initial commit

---
+++ tictactoe.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+char board_data[10] = {'o','1','2','3','4','5','6','7','8','9'};
+
+void board();
+
+int main()
+{
+	//cout<<board_data[2];
+	
+	board();
+	return 0;
+}
+
+void board()
+{
+	system("cls");
+	cout << "\n\nTic Tac Toe\n\n";
+}
